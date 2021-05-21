1f3e5e6893dc6b6776ae1a4d885d88ec918a7124
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2021-05-21 11:55:38 +0200

    Refactor e privatizzazione dell' attributo statico relativo all' api
    Signed-off-by: Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>

--- src/ytchannel.py
+++ src/ytchannel.py
@@ -29,28 +29,37 @@ class YtChannel:
     # def views(self):
     #     return json.loads(self.__data_s)["items"][0]["statistics"]["viewCount"]
 
-    yt_api_iface = ytApi(api_key=api_key)
+    # Static Attributes
+    __yt_api_iface = ytApi(api_key=api_key)
 
     def __init__(self, channel_id: str):
+        # Attributes
         self.__channel_id: str = channel_id
         self.__title: str = ""
         self.__subs_count: int = 0
         self.__views_count: int = 0
         self.__videos_count: int = 0
+
+        # Initialization
         self.__update()
 
     def __update(self):
-        channel = self.yt_api_iface.get_channel_info(channel_id=self.__channel_id, parts="statistics,snippet").items[
-            0].to_dict()
+        # Get the api handle and channel data
+        channel = YtChannel.__yt_api_iface.get_channel_info(channel_id=self.__channel_id, parts="statistics,snippet") \
+            .items[0].to_dict()
+
+        # Get Statistics info
         statistics = channel['statistics']
         self.__subs_count = statistics['subscriberCount']
         self.__views_count = statistics['viewCount']
         self.__videos_count = statistics['videoCount']
+
+        # Get channel title
         if self.__title is "":
             snippet = channel['snippet']
             self.__title = snippet['title']
 
-    # getter
+    # Getters
     @property
     def title(self):
         return self.__title
